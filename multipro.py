91f84768968ef0a0352e1456e6cf24d345247f63
louistan52-oss <louistan.52@gmail.com>
2025-10-03 14:36:06 +0800

    created working directory - Proeval

---
+++ multipro.py
@@ -0,0 +1,26 @@
+import time
+import multiprocessing
+
+def basic_func(x):
+    if x == 0:
+        return 'zero'
+    elif x%2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+def multiprocessing_func(x):
+    y = x*x
+    time.sleep(2)
+    print('{} squared results in a/an {} number'.format(x, basic_func(y>
+if ___name___ == '__main__':
+    starttime = time.time()
+    processes = []
+    for i in range(0,10):
+        p = multiprocessing.Process(target=multiprocessing_func, args=(>        processes.append(p)
+        p.start()
+
+    for process in processes:
+        process.join()
+
+    print('That took () seconds'.format(time.time() - starttime))
