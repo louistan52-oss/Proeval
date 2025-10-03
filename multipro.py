cfe56bb57eaab0937bcc672b10ec36b73a97a85e
louistan52-oss <louistan.52@gmail.com>
2025-10-03 14:46:07 +0800

    repaired multipro code

--- multipro.py
+++ multipro.py
@@ -12,12 +12,15 @@ def basic_func(x):
 def multiprocessing_func(x):
     y = x*x
     time.sleep(2)
-    print('{} squared results in a/an {} number'.format(x, basic_func(y>
-if ___name___ == '__main__':
+    print('{} squared results in a/an {} number'.format(x, basic_func(y)))
+
+
+if __name__ == '__main__':
     starttime = time.time()
     processes = []
     for i in range(0,10):
-        p = multiprocessing.Process(target=multiprocessing_func, args=(>        processes.append(p)
+        p = multiprocessing.Process(target=multiprocessing_func, args=(i,))        
+        processes.append(p)
         p.start()
 
     for process in processes:
